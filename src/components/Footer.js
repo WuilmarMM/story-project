1f15fb56d96200e484b9344e6c19b0919d4e0319
WuilmarMM <77976613+WuilmarMM@users.noreply.github.com>
2025-02-04 21:03:22 -0500

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <Footer>
+            <h2>
+                Lesson: "Guilt will eat us alive."
+            </h2>
+        </Footer>
+    );
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>The Tell-Tale Heart</h1> 
+            <h2>By Edgar Allen Poe</h2>
+        </header>  
+    );
+};
+
+export default Header;
